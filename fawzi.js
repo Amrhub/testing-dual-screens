008040d5abcdfc85928b9fc8fe51a700c3efc452
Fawzi Abdulfattah <iifawzie@gmail.com>
2023-12-17 14:23:36 +0200

    d

---
+++ fawzi.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', async () => {
+    let screenDetails;
+    if (!('getScreenDetails' in self) || !('isExtended' in screen) || !('onchange' in screen)) {
+        console.log("not supported");
+    } else {
+        console.log("nice..");
+        screen.addEventListener('change', () => { 
+
+         });
+        window.addEventListener('resize', () => { 
+            
+         });
+        permissionStatus = await navigator.permissions.query({ name: 'window-management' });
+        permissionStatus.addEventListener('change', (p) => {
+            permissionStatus = p;
+        }
+        );
+    }
+    if ('getScreenDetails' in self) {
+        if (!screenDetails && ((permissionStatus && permissionStatus.state === 'granted') ||
+            (permissionStatus && permissionStatus.state === 'prompt' && requestPermission))) {
+            screenDetails = await getScreenDetails().catch(e => { console.error(e); return null; });
+            if (screenDetails) {
+                const currentScreenLeft = screenDetails.currentScreen.left;
+                const otherScreen = screenDetails.screens.filter(screen => screen.left !== currentScreenLeft)[0];
+                window.open("https://dev-playerapp2.intouch.com", '_blank', getFeaturesFromOptions(otherScreen));
+            }
+        }
+    }
+});
+
+function getFeaturesFromOptions(options) {
+    return "left=" + options.left + ",top=" + options.top +
+           ",width=" + options.width + ",height=" + options.height +
+           (options.fullscreen ? ",fullscreen" : "");
+  }
+  
