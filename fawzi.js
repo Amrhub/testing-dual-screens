040fa3ba6e5e8a0767c8662b4c3ea7e8b0af36cf
Amr Ahmed <amr@intouch.com>
2024-03-20 13:50:31 +0200

    open player app by default on main screen

--- fawzi.js
+++ fawzi.js
@@ -1,39 +1,33 @@
 const btn = document.getElementById('test')
-btn.addEventListener('click', async () => {
-    main()
-})
-
-window.addEventListener('load', async () => {
-    main()
-});
+btn.addEventListener('click', async () => {main()})
+window.addEventListener('load', async () => {main()});
 
 async function main() {
     let screenDetails;
     if (!('getScreenDetails' in self) || !('isExtended' in screen) || !('onchange' in screen)) {
         console.log("not supported");
     } else {
-        console.log("nice..");
-        screen.addEventListener('change', () => { 
+        screen.addEventListener('change', () => {});
+        window.addEventListener('resize', () => {});
 
-         });
-        window.addEventListener('resize', () => { 
-            
-         });
         permissionStatus = await navigator.permissions.query({ name: 'window-management' });
 
         console.log({permissionStatus})
         permissionStatus.addEventListener('change', (p) => {
             permissionStatus = p;
-        }
-        );
+        });
     }
+
     if ('getScreenDetails' in self) {
+
         if (!screenDetails && ((permissionStatus && permissionStatus.state === 'granted') ||
             (permissionStatus && permissionStatus.state === 'prompt'))) {
             screenDetails = await getScreenDetails().catch(e => { console.error(e); return null; });
             if (screenDetails) {
                 const currentScreenLeft = screenDetails.currentScreen.left;
                 const otherScreen = screenDetails.screens.filter(screen => screen.left !== currentScreenLeft)[0];
+                const currentScreen = screenDetails.screens.find(screen => screen.left === currentScreenLeft);
+                window.open("https://dev-playerapp.intouch.com", undefined, getFeaturesFromOptions(currentScreen));
                 window.open("https://dev-playerapp2.intouch.com", '_blank', getFeaturesFromOptions(otherScreen));
             }
         }
